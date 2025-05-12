1f352af7cdebb4a00cebe80da83966ebf67bd6d9
tbandrowsky <todd.bandrowsky@gmail.com>
2025-05-11 20:56:45 -0500

    Add uninstall option.

--- coronaserver/coronaserver.cpp
+++ coronaserver/coronaserver.cpp
@@ -443,7 +443,41 @@ int main(int argc, char *argv[])
     {
         SvcInstall();
         return 0;
-    }
+	}
+	else if (_strcmpi(argv[1], "uninstall") == 0)
+	{
+		SC_HANDLE schSCManager;
+		SC_HANDLE schService;
+		// Get a handle to the SCM database. 
+		schSCManager = OpenSCManager(
+			NULL,                    // local computer
+			NULL,                    // ServicesActive database 
+			SC_MANAGER_ALL_ACCESS);  // full access rights 
+		if (NULL == schSCManager)
+		{
+			printf("OpenSCManager failed (%d)\n", GetLastError());
+			return 1;
+		}
+		// Get a handle to the service.
+		schService = OpenService(
+			schSCManager,   // SCM database 
+			SVCNAME,        // name of service 
+			DELETE);        // need delete access 
+		if (schService == NULL)
+		{
+			printf("OpenService failed (%d)\n", GetLastError());
+			CloseServiceHandle(schSCManager);
+			return 1;
+		}
+		// Delete the service.
+		if (!DeleteService(schService))
+		{
+			printf("DeleteService failed (%d)\n", GetLastError());
+		}
+		else printf("Service deleted successfully\n");
+		CloseServiceHandle(schService);
+		CloseServiceHandle(schSCManager);
+	}
     else if (_strcmpi(argv[1], "console") == 0)
     {
         RunConsole();
