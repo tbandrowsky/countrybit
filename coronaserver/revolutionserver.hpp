5358de83df73b8c39f528bf86e36bacd4f642a70
tbandrowsky <todd.bandrowsky@gmail.com>
2025-05-29 23:45:28 -0500

    fleshing out core application.

--- coronaserver/revolutionserver.hpp
+++ coronaserver/revolutionserver.hpp
@@ -14,19 +14,170 @@ namespace corona::apps
         {
             std::string class_name = _command["class_name"];
 
-            if (class_name == "")
+            /*
+                {
+      "class_name": "select_clear_command",
+      "class_description": "command for one piece to activate another",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor"
+      }
+    },
+    {
+      "class_name": "select_extend_command",
+      "class_description": "command for one piece to activate another",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor",
+        "target_actor_id": "[ ->actor ]"
+      }
+    },
+    {
+      "class_name": "compose_command",
+      "class_description": "command to consume selected pieces to create another",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor",
+        "part_type": "string"
+      }
+    },
+    {
+      "class_name": "take_command",
+      "class_description": "command to consume selected pieces to create another",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor",
+        "part_type": "string"
+      }
+    },
+    {
+      "class_name": "drop_command",
+      "class_description": "command to consume selected pieces to create another",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor",
+        "part_type": "string"
+      }
+    },
+    {
+      "class_name": "trade_command",
+      "class_description": "command to consume selected pieces to create another",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor",
+        "counterparty": "->actor",
+        "duecounterparty": "[ ->actor ]",
+        "dueparty": "[ ->actor ]",
+        "approval_party": "string",
+        "approval_counterparty": "string"
+      }
+    },
+    {
+      "class_name": "accelerate_command",
+      "class_description": "command to move selected pieces",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor",
+        "target_actor_id": "->actor",
+        "ax": "double",
+        "ay": "double",
+        "az": "double"
+      }
+    },
+    {
+      "class_name": "activate_command",
+      "class_description": "command for one piece to activate another",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor",
+        "target_actor_id": "->actor"
+      }
+    },
+    {
+      "class_name": "navigate_command",
+      "class_description": "",
+      "base_class_name": "sys_command",
+      "fields": {
+        "board_id": "->board",
+        "page_id": "->page",
+        "actor_id": "->actor"
+      }
+    },
+    {
+      "class_name": "join_game_command",
+      "class_description": "command to join a game",
+      "base_class_name": "sys_command",
+      "fields": {
+        "game_id": "->game"
+      }
+    },
+    {
+      "class_name": "exit_game_command",
+      "class_description": "command to leave a game",
+      "base_class_name": "sys_command",
+      "fields": {
+        "game_id": "->game"
+      }
+    }
+            */
+
+            if (class_name == "select_clear_command")
             {
                 ;
             }
-            else if (class_name == "")
+            else if (class_name == "select_extend_command")
             {
                 ;
             }
-            else if (class_name == "")
+            else if (class_name == "compose_command")
+            {
+                _service->get_data;
+            }
+            else if (class_name == "take_command")
             {
                 ;
             }
-            else if (class_name == "")
+            else if (class_name == "drop_command")
+            {
+                ;
+            }
+            else if (class_name == "trade_command")
+            {
+                ;
+            }
+            else if (class_name == "accelerate_command")
+            {
+                ;
+            }
+            else if (class_name == "activate_command")
+            {
+                ;
+            }
+            else if (class_name == "navigate_command")
+            {
+                ;
+            }
+            else if (class_name == "join_game_command")
+            {
+                ;
+            }
+            else if (class_name == "exit_game_command")
             {
                 ;
             }
