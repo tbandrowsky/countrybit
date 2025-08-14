e946b9c9c1632b3a58ad449a3ee3f7287b748b2c
Todd Bandrowsky <todd.bandrowsky@gmail.com>
2025-08-14 12:43:12 -0500

    Update and compile.

--- coronaserver/postbuild.bat
+++ coronaserver/postbuild.bat
@@ -3,5 +3,6 @@ DEL  /Q .\distribution\*.*
 COPY ..\x64\Release\coronaserver.exe .\distribution\coronaserver.exe
 COPY ..\x64\Release\CoronaApplicationServer.docx .\distribution\CoronaApplicationServer.docx
 COPY ..\x64\Release\*.json .\distribution\
+COPY ..\x64\Release\*.html .\distribution\
 COPY dockerfile .\distribution\
 docker build -t tbandrow/corona .
